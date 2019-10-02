23b43935b04d711ca084da3f5638bed9e16824a8
rishabhojha <rishabh.education.123@gmail.com>
2019-10-02 17:21:11 +0530

    Corrected the syntax error in sqlalchemy import statement

--- import.py
+++ import.py
@@ -1,10 +1,10 @@
 import os
 
-from sqlalchemy import create_session
+from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 def import_books ():
-    engine = create_session(os.getenv("DATABASE_URL"))
+    engine = create_engine(os.getenv("DATABASE_URL"))
     db = scoped_session(sessionmaker(bind=engine))
 
     books_csv = open ("books.csv")
